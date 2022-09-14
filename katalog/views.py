76194f2e9ed51c68046a879111f8634aa8726da5
ShafanisaAlifia <Shafanisa.alifia@ui.ac.id>
2022-09-14 12:18:48 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    list_item = CatalogItem.objects.all()
+    context = {
+    'list_item': list_item,
+    'nama' : 'Alifia',
+    'NPM': '2106634723'
+}
+    return render(request, "katalog.html", context)
+
